dd8484b14a3b835b052c12e38eadea79cf918b96
Oni-210 <olenienaber2010@gmail.com>
2026-06-25 08:24:13 +0200

    add expression enums

--- src/visual_basics.h
+++ src/visual_basics.h
@@ -1,7 +1,27 @@
 #include <Arduino.h>
+##include <Enum.h>
 
 struct Color {
     byte red;
     byte green;
     byte blue;
 };
+
+enum class Expressions {
+	neutral,
+    happy,
+    sad,
+    annoyed,
+    insult,
+    error,
+    dead,
+    confused,
+    beatsync,
+    shy
+};
+
+enum class TempExpression {
+	wink,
+    blink,
+	social_battery
+}
