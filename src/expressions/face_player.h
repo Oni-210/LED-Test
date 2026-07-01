bcb4f10556a58edea54476d2f1b568a105e2e9a3
Oni-210 <195586055+Oni-210@users.noreply.github.com>
2026-07-01 09:40:00 +0200

    Added blinking animation

--- src/expressions/face_player.h
+++ src/expressions/face_player.h
@@ -16,6 +16,9 @@ const float SOUND_LEVELS[7] = {
     0.7
 };
 
+const unsigned int BLINK_GAP_MAX = 3000;
+const unsigned int BLINK_GAP_MIN = 500;
+
 // GUItool: begin automatically generated code
 extern AudioInputI2S            i2s;           //xy=303,297
 extern AudioAnalyzeRMS          rms;           //xy=523,251
@@ -25,12 +28,21 @@ extern AudioConnection          patchCord1;
 class FacePlayer : public Visual {
     public:
         FacePlayer(char* eye_path, char* mouth_path, char* talking_path);
+        FacePlayer(char* eye_path, char* mouth_path, char* talking_path, char* blink_path);
         ~FacePlayer();
         std::unique_ptr<Color[]> run() override;
     private:
         File eye_file;
         File mouth_file;
         File talking_file;
+        File blink_file;
+        bool is_blink;
+        bool currently_blinking = false;
+        unsigned int current_blink_frame = 0;
+        unsigned int blink_frame_count;
+        elapsedMillis since_last_blink = 0;
+        unsigned int blink_gap;
         unsigned int current_frame = 0;
         unsigned int frame_count;
+        
 };
